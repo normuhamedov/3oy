4a07e738313370950d150da6d8783c7c19f81906
normuhamedov <normuhamedov22@gmail.com>
2022-12-16 20:25:57 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+var year = prompt("Yil", "2022");
+var firstName = prompt("Ismingizni kiriting");
+var familia = prompt("Familiyainggizni kiriting");
+var age = prompt("Yoshingizni kiriting");
+var manzil = prompt("Manzilingizni kiriting");
+var tel = prompt("Raqaminggizni kiriting");
+var lavozim = prompt("Lavozimingizni kiriting");
+var millat = prompt("Millatingizni kiriting");
+var daraja = prompt("Darajangizni kiriting");
+var nimadur = prompt("Nimaduringgizni kiriting");
+
+
+var fullname = 
+"yil: " + year + "\n" +
+"Ismingiz: " + firstName + "\n" +
+"Familiyainggiz:" + familia + "\n" +
+"Yoshingiz: " + age + "\n" +
+"Manzilingiz: " + manzil + "\n" +
+"Raqaminggiz: " + tel + "\n" +
+"Lavozimingiz: " + lavozim + "\n" +
+"Millatingiz: " + millat + "\n" +
+"Darajangiz: " + daraja + "\n" +
+"Nimaduringgiz: " + nimadur;
+
+
+alert(fullname);
+console.log(fullname);
